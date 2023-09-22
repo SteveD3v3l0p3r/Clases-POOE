6251720868c6bcb5b81400a1cac60788e0115bed
Gerson Ramirez <gersonstever@gmail.com>
2023-09-21 19:46:32 -0600

    Calculo de notas de computo

--- ConsoleApp1/ConsoleApp1/Module1.vb
+++ ConsoleApp1/ConsoleApp1/Module1.vb
@@ -1,12 +1,20 @@
 ﻿Module Module1
 
     Sub Main()
-        Console.WriteLine("Hola mundo")
-        Dim nombre As String
-        nombre = "Gerson Ramírez"
+        'Lab1 = 30%, Lab2 = 30%, P = 40% = NF COMPUTO'
+        Dim lab1, lab2, parcial, nf_computo As Double
 
-        Console.WriteLine("Hola " & nombre)
+        Console.Write("Digite la nota del laboratorio 1: ")
+        lab1 = Console.ReadLine()
 
+        Console.Write("Digite la nota del laboratorio 2: ")
+        lab2 = Console.ReadLine()
+
+        Console.Write("Digite la nota del parcial: ")
+        parcial = Console.ReadLine()
+
+        nf_computo = (lab1 * 0.3 + lab2 * 0.3 + parcial * 0.4)
+        Console.Write("Su nota final del computo es: " & nf_computo)
         Console.ReadKey()
     End Sub
 
