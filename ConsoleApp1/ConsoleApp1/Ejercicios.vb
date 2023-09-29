b8d3e8ee2e5f252ad9da734a5c1f755ed1dd8c46
Gerson Ramirez <gersonstever@gmail.com>
2023-09-29 15:17:55 -0600

    encontrar el mayor de los numeros

---
+++ ConsoleApp1/ConsoleApp1/Ejercicios.vb
@@ -0,0 +1,33 @@
+﻿Public Class Ejercicios
+    Public Sub secuenciaFibonacci()
+        Console.Write("Valor maximo de la secuencia: ")
+        Dim max = Console.ReadLine()
+
+        Dim i As Integer
+        Dim a = 1, b = 0, c = 1
+        While c <= max
+            c = a + b
+            Console.WriteLine(a.ToString() & " + " & b.ToString() & " = " & c.ToString())
+            b = a
+            a = c
+        End While
+    End Sub
+
+    'Determinar el mayor de 5 numeros'
+    Public Function getMayor()
+        Dim num_mayor = 0
+        Dim i = 1
+
+        While i <= 5
+            Console.Write("Ingresa un numero: ")
+            Dim num = Console.ReadLine()
+
+            If num > num_mayor Then
+                num_mayor = num
+            End If
+            i += 1
+        End While
+
+        Return num_mayor
+    End Function
+End Class
--- ConsoleApp1/ConsoleApp1/Module1.vb
+++ ConsoleApp1/ConsoleApp1/Module1.vb
@@ -1,47 +1,11 @@
 ﻿Module Module1
 
     Sub Main()
-        Dim objEstudiante1 = New Estudiante()
-        Dim respuesta As Char
+        Dim objeto = New Ejercicios()
 
-        Console.WriteLine("***** UNIVERSIDAD GERADO BARRIOS *****")
+        'objeto.secuenciaFibonacci()'
+        Console.WriteLine("El numero mayor es: " & objeto.getMayor())
 
-        Console.Write("¿Desea ingresar notas? Si (s) No (n)")
-        Console.WriteLine()
-        respuesta = Console.ReadLine().ToLower()
-        While respuesta = "s"
-            Console.Write("Carrera: ")
-            objEstudiante1.setCarrera(Console.ReadLine().ToString())
-            Console.Write("Apellidos: ")
-            objEstudiante1.setApellidos(Console.ReadLine().ToString())
-            Console.Write("Nombres: ")
-            objEstudiante1.setNombre(Console.ReadLine().ToString())
-            Console.Write("Codigo: ")
-            objEstudiante1.setCodigo(Console.ReadLine().ToString())
-            Console.Write("Materia: ")
-            objEstudiante1.setMateria(Console.ReadLine().ToString())
-            Console.WriteLine("*************************************")
-
-            Console.Write("Digite su nota 1: ")
-            objEstudiante1.setNota1(Console.ReadLine())
-            Console.Write("Digite su nota 2: ")
-            objEstudiante1.setNota2(Console.ReadLine())
-            Console.Write("Digite su nota 3: ")
-            objEstudiante1.setNota3(Console.ReadLine())
-            Console.Write("Digite su nota 4: ")
-            objEstudiante1.setNota4(Console.ReadLine())
-
-            Console.WriteLine("*************************************")
-            Console.WriteLine("Alumno: " & objEstudiante1.getApellidos() & " " & objEstudiante1.getNombre())
-            Console.WriteLine("Codigo de alumno: " & objEstudiante1.getCodigo())
-            Console.WriteLine("Carrera: " & objEstudiante1.getCarrera())
-            Console.WriteLine("Materia: " & objEstudiante1.getMateria())
-            Console.WriteLine("Nota final de la materia: " & objEstudiante1.getNotaFinal())
-
-            Console.Write("¿Desea ingresar notas? Si (s) No (n)")
-            Console.WriteLine()
-            respuesta = Console.ReadLine().ToLower()
-        End While
         Console.ReadKey()
     End Sub
 
